ca113b4adbc0fd30613ca1efec284e1af5aa744c
Izzatbek <abdusharipovizzat03@gmail.com>
2026-02-18 23:44:58 +0500

    React Modal Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg_image text-white'>
+      <Navbar/>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { UserCircle } from "lucide-react";
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <motion.div className="fixed top-0 left-0 w-full flex items-center justify-around gap-3 z-40 p-4 bg-gray-900 opacity-80">
+      <div className="">
+        <motion.h1 
+        
+        className="text-lg font-semibold md:text-xl
+         cursor-pointer">
+          Izzatbek <span className="text-pink-600">Abdusharipov</span>
+        </motion.h1>
+      </div>
+      <div className="hidden md:block">
+        <ul className="flex items-center gap-4 sm:gap-6 md:gap-8 lg:gap-10">
+          <li className="cursor-pointer hover:text-pink-500 transition duration-300 font-medium">Home</li>
+          <li className="cursor-pointer hover:text-pink-500 transition duration-300 font-medium">About</li>
+          <li className="cursor-pointer hover:text-pink-500 transition duration-300 font-medium">Service</li>
+          <li className="cursor-pointer hover:text-pink-500 transition duration-300 font-medium">Project</li>
+          <li className="cursor-pointer hover:text-pink-500 transition duration-300 font-medium">Contact</li>
+        </ul>
+      </div>
+      <div className="text-pink-500 cursor-pointer hover:text-white">
+        <UserCircle />
+      </div>
+    </motion.div>
+  );
+};
 
-export default Navbar
+export default Navbar;
